c57e8d91b1ea838a1b19ded5ce85318b8f3116f5
Space1255 <155597557+Space1255@users.noreply.github.com>
2025-10-27 10:34:20 -0400

    Menu Stuff Coded
    Created menu scripts, updated build settings, used the test scene as a stand-in for the actual level scene

---
+++ Silver Bullet/Assets/Scripts/Menu Scripts/TryAgain.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class TryAgain : MonoBehaviour
+{
+    public Button button;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Button buttonbutton = gameObject.GetComponent<Button>();
+        buttonbutton.onClick.AddListener(Buttoned);
+        //I have decided to pursue 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void Buttoned()
+    {
+        Debug.Log("Main Menu fired from " + gameObject.name);
+        SceneManager.LoadScene(3);
+    }
+}

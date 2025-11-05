4c2b7609d4775158c560c1286db848a70c835c21
Space1255 <155597557+Space1255@users.noreply.github.com>
2025-11-05 08:56:16 -0500

    More Invuln Frame functionality
    imagine i said something funny here

--- Silver Bullet/Assets/Scripts/PlayerHealth.cs
+++ Silver Bullet/Assets/Scripts/PlayerHealth.cs
@@ -10,13 +10,40 @@ public class PlayerHealth : MonoBehaviour
     public int maxHealth;
     public Slider healthBar;
     [SerializeField] float invulnTime;
+    public float invulnWait;
     public bool invuln;
+    public float fear;
     // Start is called before the first frame update
     void Start()
     {
         currentHealth = maxHealth;
         healthBar.maxValue = maxHealth;
         healthBar.value = currentHealth;
+        fear = 0;
+    }
+
+    void Update()
+    {
+        if (fear < 0)
+        {
+            fear = 0f;
+        }
+        
+        if (fear >= 0)
+        {
+            if (fear >= invulnTime)
+            {
+                invulnWait = invulnTime - invulnTime;
+                //Basically reduces invuln time to zero without underflowing
+                //put stuff in here to interact with the fear meter
+            }
+            else
+            {
+                invulnWait = invulnTime - fear;
+                //Otherwise, reduces invuln wait by the normal value
+                //put stuff in here to interact with the fear meter
+            }
+        }
     }
 
     public void PlayerTakeDamage(int amount)
@@ -42,12 +69,38 @@ public class PlayerHealth : MonoBehaviour
     {
         invuln = true;
         Debug.Log("Invuln On");
-        Invoke("InvulnOff", invulnTime);
+        Invoke("InvulnOff", invulnWait);
     }
 
     void InvulnOff()
     {
         invuln = false;
         Debug.Log("Invuln Off");
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Fear"))
+        {
+            fear = fear + 0.1f;
+            Debug.Log("Fear at " + fear);
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Fear"))
+        {
+            if (fear > 0)
+            {
+                fear = fear - 0.1f;
+                Debug.Log("Fear at " + fear);
+            }
+            if (fear <= 0)
+            {
+                fear = 0;
+                Debug.Log("Fear would underflow to 0, setting fear to 0.");
+            }
+        }
     }
 }   

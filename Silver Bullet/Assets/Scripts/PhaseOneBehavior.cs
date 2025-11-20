0b98ccf3ea1d88d062a9dd4e7c179bc8cbbdbb4b
DatBoiDean <dean.b.collins@gmail.com>
2025-11-20 13:27:45 -0500

    Attempted boss functionality

--- Silver Bullet/Assets/Scripts/PhaseOneBehavior.cs
+++ Silver Bullet/Assets/Scripts/PhaseOneBehavior.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class PhaseOneBehavior : MonoBehaviour
 {
+    public EnemyHealth enemyHealth;
+    [SerializeField] Rigidbody2D obstacle;
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log("Phase 1 Triggered");
+        Rigidbody2D spawnedObstacle = Instantiate(obstacle, transform.position, transform.rotation);
+        spawnedObstacle.velocity = transform.TransformDirection(Vector2.up * 2);
+
+        Invoke("DestroyOnBossHit", 2f); 
     }
 
     // Update is called once per frame
-    void Update()
+
+
+    void DestoryOnBossHit(Rigidbody2D spawnedObstacle) 
     {
-        
+        if (enemyHealth.currentEnemyHealth == 2)
+        {
+            Destroy(spawnedObstacle);
+        }
     }
 }
+

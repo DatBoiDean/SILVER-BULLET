a7210f149fba05d9fa665413379fa9c60aebd756
Space1255 <155597557+ERAlexanderUbalt@users.noreply.github.com>
2025-11-27 17:06:57 -0500

    I've dehydrated Eggman

--- Silver Bullet/Assets/Scripts/PhaseTwoBehavior.cs
+++ Silver Bullet/Assets/Scripts/PhaseTwoBehavior.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
 using UnityEngine;
 
 public class PhaseTwoBehavior : MonoBehaviour
@@ -9,17 +10,32 @@ public class PhaseTwoBehavior : MonoBehaviour
     [SerializeField] float spawnDelay = 1f;
     // Start is called before the first frame update
 
-    IEnumerator PhaseOneRoutine()
+
+    void Start()
     {
-        while (enemyHealth.currentEnemyHealth == 2) // spawn this type of obstacle while boss health is full
-        {
-            Instantiate(obstacleToSpawn, transform.position, Quaternion.identity);
-            yield return new WaitForSeconds(spawnDelay);
-        }
+        Run();
     }
 
-    private void Update()
+    void Run()
     {
-        StartCoroutine(PhaseOneRoutine());
+        if (enemyHealth.currentEnemyHealth == 2)
+        {
+            Instantiate(obstacleToSpawn, transform.position, Quaternion.identity);
+        }
+            Invoke("Run",spawnDelay);
+        
     }
+    // IEnumerator PhaseOneRoutine()
+    // {
+    //     while (enemyHealth.currentEnemyHealth == 2) // spawn this type of obstacle while boss health is full
+    //     {
+    //         Instantiate(obstacleToSpawn, transform.position, Quaternion.identity);
+    //         yield return new WaitForSeconds(spawnDelay);
+    //     }
+    // }
+
+    // private void Update()
+    // {
+    //     StartCoroutine(PhaseOneRoutine());
+    // }
 }

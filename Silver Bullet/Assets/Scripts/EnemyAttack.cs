15deb57a7f29eb26cefd725b8a7dabea3cfec88a
Space1255 <155597557+ERAlexanderUbalt@users.noreply.github.com>
2025-11-26 20:22:33 -0500

    Changed method of attacking
    based on distance instead of triggers

--- Silver Bullet/Assets/Scripts/EnemyAttack.cs
+++ Silver Bullet/Assets/Scripts/EnemyAttack.cs
@@ -2,17 +2,25 @@ using UnityEngine;
 
 public class EnemyAttack : MonoBehaviour
 {
+    [SerializeField] float attackRange;
     public int damageInterval;
     public int damageAmount;
     public float nextDamageTime;
-
-    private void OnTriggerStay2D(Collider2D other)
+    public GameObject player;
+void Start()
     {
-        if (other.CompareTag("Player"))  //check if collision has player tag
+        player = GameObject.Find("Player"); 
+    }
+
+void Update()
+    {
+        //This method uses distance measuring to attack the player, instead of simply being in contact with the collider
+        float dist = Vector3.Distance(player.transform.position, transform.position);
+        if (dist <= attackRange)
         {
             if (Time.time >= nextDamageTime)
             {
-                var playerHealthComponent = other.GetComponent<PlayerHealth>();
+                var playerHealthComponent = player.GetComponent<PlayerHealth>();
 
                 if (playerHealthComponent != null)
                 {
@@ -23,13 +31,40 @@ public class EnemyAttack : MonoBehaviour
                 }
             }
         }
-    }
 
-    private void OnTriggerExit2D(Collider2D other)
-    {
-        if (other.CompareTag("Player"))
+        if (dist > attackRange)
         {
             nextDamageTime = 0;
         }
     }
+
+        //Commented all of this out so I can do a distance measure for attacking range,
+        //      instead of using triggers. 
+
+    // private void OnTriggerStay2D(Collider2D other)
+    // {
+    //     if (other.CompareTag("Player"))  //check if collision has player tag
+    //     {
+    //         if (Time.time >= nextDamageTime)
+    //         {
+    //             var playerHealthComponent = other.GetComponent<PlayerHealth>();
+
+    //             if (playerHealthComponent != null)
+    //             {
+    //                 Debug.Log("Player health component found");
+    //                 playerHealthComponent.PlayerTakeDamage(damageAmount);
+
+    //                 nextDamageTime = Time.time + damageInterval;
+    //             }
+    //         }
+    //     }
+    // }
+
+    // private void OnTriggerExit2D(Collider2D other)
+    // {
+    //     if (other.CompareTag("Player"))
+    //     {
+    //         nextDamageTime = 0;
+    //     }
+    // }
 }

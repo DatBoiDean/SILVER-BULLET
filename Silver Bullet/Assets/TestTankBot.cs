2bda3d47ca1bba315b4261e1c7f383c2478cd508
DatBoiDean <dean.b.collins@gmail.com>
2026-02-23 17:33:08 -0500

    Started TankBot Implementation

---
+++ Silver Bullet/Assets/TestTankBot.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestTankBot : MonoBehaviour
+{
+    [SerializeField] float moveSpeed;
+    [SerializeField] GameObject player;
+    [SerializeField] Rigidbody2D tankBotRB;
+    private Transform playerTransform;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

ff9c8be16f8374e7a58e31ce66a73fe53a6e982e
DatBoiDean <dean.b.collins@gmail.com>
2026-03-01 22:52:00 -0500

    tank bot now moves

--- Silver Bullet/Assets/TestTankBot.cs
+++ Silver Bullet/Assets/TestTankBot.cs
@@ -5,18 +5,28 @@ using UnityEngine;
 public class TestTankBot : MonoBehaviour
 {
     [SerializeField] float moveSpeed;
-    [SerializeField] GameObject player;
+    [SerializeField] Transform player;
     [SerializeField] Rigidbody2D tankBotRB;
-    private Transform playerTransform;
+
+    public bool flip;
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 scale = transform.localScale;
+
+        if (player.transform.position.x > transform.position.x) //check if player's position is greater than enemy's position
+        {
+            scale.x = Mathf.Abs(scale.x) * -1 * (flip? -1: 1);
+            transform.Translate(moveSpeed * Time.deltaTime, 0, 0);
+        }
+        else
+        {
+            scale.x = Mathf.Abs(scale.x) * (flip ? -1 : 1);
+            transform.Translate(moveSpeed * Time.deltaTime * -1, 0, 0);
+        }
+
+        transform.localScale = scale;
     }
 }

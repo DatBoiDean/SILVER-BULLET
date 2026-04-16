f25b7b15b919df85082e56e4611d0234c03c7943
DatBoiDean <dean.b.collins@gmail.com>
2026-04-16 00:30:59 -0400

    grab reset on damage now works properly

--- Silver Bullet/Assets/WireManAttack.cs
+++ Silver Bullet/Assets/WireManAttack.cs
@@ -7,7 +7,7 @@ public class WireManAttack : MonoBehaviour
     [SerializeField] float grabRange = 5f;
     [SerializeField] GameObject grabZone;
     [SerializeField] GameObject player;
-    [SerializeField] float grabResetTimer;
+    
     [SerializeField] int damageAmount = 1;
 
     bool playerTookDamage = false;
@@ -29,8 +29,6 @@ public class WireManAttack : MonoBehaviour
             Debug.Log("Player not in range");
         }
 
-        resetOnDamage();
-
     }
 
     private void OnCollisionEnter2D(Collision2D collision) //damages player on contact with wire man
@@ -43,20 +41,19 @@ public class WireManAttack : MonoBehaviour
             playerHealthComponent.PlayerTakeDamage(damageAmount);
 
             playerTookDamage = true;
+
+            if (playerTookDamage)
+            {
+                StartCoroutine(resetOnDamage());
+            }
         }
     }
 
-    void resetOnDamage() //resets the grab so the player can escape the grab zone
+    IEnumerator resetOnDamage()
     {
-        if (playerTookDamage)
-        {
-            grabRange = -10f; //prevents grab zone from being active next to player
-            grabResetTimer = Time.time;
-
-            if (grabResetTimer >= 5f)
-            {
-                grabRange = 5f;
-            }
-        }
+        Debug.Log("Reset on damage started");
+        grabRange = -10f;
+        yield return new WaitForSeconds(5f);
+        grabRange = 5f;
     }
 }

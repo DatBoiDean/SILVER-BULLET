5f18130482c09b26fa8608554c8debcfe0152a26
Space1255 <155597557+ERAlexanderUbalt@users.noreply.github.com>
2026-05-04 11:55:04 -0400

    Ground Button implemented

---
+++ Silver Bullet/Assets/buttonpress.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buttonpress : MonoBehaviour
+{
+    [SerializeField] GameObject target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Destroy(target);
+        Destroy(gameObject);
+    }
+}
